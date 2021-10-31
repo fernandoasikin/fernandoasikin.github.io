0cad530db9d865b14cc2f2e28543c8794af4997a
Fernando Asikin <fernando.asikin@binus.ac.id>
2021-10-31 10:22:25 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const hamburgermenu = document.querySelector('#nagivation .iconnagivation');
+
+const contentnagivation = document.querySelector('#contentnagivation');
+
+const closenagivationbutton =  document.querySelector('#contentnagivation .closebutton');
+
+const scrollButton = document.querySelector('.scroll-top')
+
+
+if(scrollButton){
+    window.addEventListener('scroll', ()=>{
+        if(pageYOffset > (window.innerHeight *1.2)){
+            scrollButton.style.display = 'flex';
+        }
+        else{
+            scrollButton.style.display = 'none';
+        }
+    })
+    scrollButton.addEventListener('click', ()=>{
+        window.scrollTo(0,0)
+    })
+}
+
+
+hamburgermenu.addEventListener('click', ()=>{
+    contentnagivation.classList.add('show');
+    document.body.style.overflow="hidden";
+})
+
+
+closenagivationbutton.addEventListener('click', ()=>{
+    contentnagivation.classList.remove('show');
+    document.body.style.overflow="auto";
+})
+
+
