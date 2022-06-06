9acfb9ffd4a0ac6e7f03bd40902f656a26686f1f
fernandoasikin <fernando.asikin@binus.ac.id>
2022-06-07 01:28:20 +0700

    Updated Portofolio Web June 2022

--- main.js
+++ main.js
@@ -34,31 +34,98 @@ closenagivationbutton.addEventListener('click', ()=>{
 })
 
 
-var slidePosition = 1;
-SlideShow(slidePosition);
+// var slidePosition = 1;
+// SlideShow(slidePosition);
+
+// // forward/Back controls
+// function plusSlides(n) {
+//   SlideShow(slidePosition += n);
+// }
+
+// //  images controls
+// function currentSlide(n) {
+//   SlideShow(slidePosition = n);
+// }
+
+// function SlideShow(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("Containers");
+//   var circles = document.getElementsByClassName("dots");
+//   if (n > slides.length) {slidePosition = 1}
+//   if (n < 1) {slidePosition = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < circles.length; i++) {
+//       circles[i].className = circles[i].className.replace(" enable", "");
+//   }
+//   slides[slidePosition-1].style.display = "block";
+//   circles[slidePosition-1].className += " enable";
+// } 
+
+
+// new
+
+var slidePosition2 = 1;
+SlideShow2(slidePosition2);
 
 // forward/Back controls
-function plusSlides(n) {
-  SlideShow(slidePosition += n);
+function plusSlides2(o) {
+  SlideShow2(slidePosition2 += o);
 }
 
 //  images controls
-function currentSlide(n) {
-  SlideShow(slidePosition = n);
+function currentSlide2(o) {
+  SlideShow2(slidePosition2 = o);
 }
 
-function SlideShow(n) {
-  var i;
-  var slides = document.getElementsByClassName("Containers");
-  var circles = document.getElementsByClassName("dots");
-  if (n > slides.length) {slidePosition = 1}
-  if (n < 1) {slidePosition = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+function SlideShow2(o) {
+  var j;
+  var slides2 = document.getElementsByClassName("Containers2");
+  var circles2 = document.getElementsByClassName("dots2");
+  if (o > slides2.length) {slidePosition2 = 1}
+  if (o < 1) {slidePosition2 = slides2.length}
+  for (j = 0; j < slides2.length; j++) {
+      slides2[j].style.display = "none";
   }
-  for (i = 0; i < circles.length; i++) {
-      circles[i].className = circles[i].className.replace(" enable", "");
+  for (j = 0; j < circles2.length; j++) {
+      circles2[j].className = circles2[j].className.replace(" enable", "");
   }
-  slides[slidePosition-1].style.display = "block";
-  circles[slidePosition-1].className += " enable";
+  slides2[slidePosition2-1].style.display = "block";
+  circles2[slidePosition2-1].className += " enable";
 } 
+
+
+
+
+// new
+
+var slidePosition3 = 1;
+SlideShow3(slidePosition3);
+
+// forward/Back controls
+function plusSlides3(p) {
+  SlideShow3(slidePosition3 += p);
+}
+
+//  images controls
+function currentSlide3(p) {
+  SlideShow3(slidePosition3 = p);
+}
+
+function SlideShow3(p) {
+  var k;
+  var slides3 = document.getElementsByClassName("Containers3");
+  var circles3 = document.getElementsByClassName("dots3");
+  if (p > slides3.length) {slidePosition3 = 1}
+  if (p < 1) {slidePosition3 = slides3.length}
+  for (k = 0; k < slides3.length; k++) {
+      slides3[k].style.display = "none";
+  }
+  for (k = 0; k < circles3.length; k++) {
+      circles3[k].className = circles3[k].className.replace(" enable", "");
+  }
+  slides3[slidePosition3-1].style.display = "block";
+  circles3[slidePosition3-1].className += " enable";
+} 
+
